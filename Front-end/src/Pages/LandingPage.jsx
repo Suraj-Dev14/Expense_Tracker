ad1869e8f23dc584ac143c01d139b2f20b54bdfa
Suraj-Dev14 <surajgaikwad141103@gmail.com>
2025-07-17 04:14:25 +0530

    Test 2

--- Front-end/src/Pages/LandingPage.jsx
+++ Front-end/src/Pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button.jsx";
 import { Card, CardContent } from "@/components/ui/card.jsx";
 import { Badge } from "@/components/ui/badge.jsx";
 import {
