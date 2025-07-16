3a51be6fbedeefba6d2bc32445ca18c418f2d576
Suraj-Dev14 <surajgaikwad141103@gmail.com>
2025-07-17 03:55:20 +0530

    First Version

--- Front-end/src/Layout.jsx
+++ Front-end/src/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
+import NavigationBar from "./components/NavigationBar.jsx";
 
 const Layout = () => {
   return (
